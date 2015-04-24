85146f8d7a276797301c60166450fec1634709af
Prabuddh Gupta <prabuddh@prabuddh-mbp.attlocal.net>
2015-04-23 21:35:57 -0700

    Added dynamic importing of modules
    Next: need to fix dynamic creation of objects

--- switch/switch_init.py
+++ switch/switch_init.py
@@ -1,7 +1,7 @@
 import os
 import yaml
 import imp
-import interface.net_interface as net_interface
+import interface.net_interface
 
 moduleNames = ['interface']
 dir = os.path.dirname(__file__)
@@ -14,6 +14,7 @@ class Switch_Init():
     def read_specification(self):
         print "Reading specification..."
         # hardcoded the spec
+        # /Users/prabuddh/Desktop/mountCode/net-switch/
         spec_yaml =\
             os.path.join(dir, '../specification/spec.yaml')
         stream = open(spec_yaml, 'r')
@@ -30,14 +31,27 @@ class Switch_Init():
         module_dict = yaml.load(stream)
         # hardcoded switch module
         class_type = spec['type']
-        module_file = module_dict[module][class_type]['module']
+        module_file = (module + "." +
+                      module_dict[module][class_type]['module'])
         class_name = module_dict[module][class_type]['name']
-        print class_name
+        self.acquire_resources(module_file, class_name, spec)
+
+    def acquire_resources(self, module_file = None, class_name = None, spec = None):
+        print ("Acquiring resources using module_file: %s and class_name: %s"
+               %(module_file, class_name))
         print module_file
-        module_object = net_interface.NetInterface(spec['name'], spec['mac'],spec['state'])
+        as_class = self.get_class(module_file)
+        # Make this dynamic
+        module_object = as_class.NetInterface(spec['name'], spec['mac'],spec['state'])
         print module_object
 
-    def acquire_resources(self):
-        print "Acquiring resources..."
+    def get_class(self, kls = None):
+        parts = kls.split('.')
+        module = ".".join(parts[:-1])
+        m = __import__( module )
+        for comp in parts[1:]:
+            m = getattr(m, comp)
+            return m
+
     def store_objects(self):
         print "Storing object..."
